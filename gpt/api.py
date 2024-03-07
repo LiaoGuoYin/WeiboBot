1796a927bf5e9fba8eb7cfb1fd1c23c2c271e817
Coin <liaoguoyin@live.com>
2024-03-07 21:26:35 +0800

    update: prompt for better output content

---
+++ gpt/api.py
@@ -0,0 +1,66 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MODEL = "gpt-3.5-turbo"
+# https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo
+REPLY_PROMPT = """
+你是没关系Bot。你的风格是没脸没皮、调侃自黑，明知道屁也不是，硬要说很厉害了的感觉。
+当我给你一段文字时，你需要提取出一件普通但是正向的事情xxxx，硬要把它说得很厉害。
+回答不能超过20个字，回答格式：没关系，xxxx，已经很厉害了。需要替换其中的xxxx
+"""
+
+AUTO_PROMPT = """
+生成一句不多于20字的积极鼓励的冷幽默短句, 不要人称代词, 不要比喻, 不要形容词, 需要客观简单些
+风格是没脸没皮、调侃自黑，明知道啥也不是，但就是要说得很厉害的感觉。
+这些短句应该强调即便是看似微小的行为或成就。成就可能是解决了工作、学习、日常生活中的小挑战、或心理上的负担。
+请确保每个短句都以“没关系，”开头，并在结尾强调“已经很厉害了”
+"""
+
+
+def no_matter_bot(
+    message="今天一下午的时间全用来搞AI了，论文一个字没写",
+    prompt=REPLY_PROMPT,
+    model=MODEL,
+):
+    # https://platform.openai.com/docs/api-reference/making-requests
+    response = requests.post(
+        os.getenv("GPT_API_URL"),
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {os.getenv('GPT_API_KEY')}",
+        },
+        json={
+            "model": MODEL,
+            "messages": [
+                {
+                    "role": "system",
+                    "content": prompt,
+                },
+                {"role": "user", "content": message},
+            ],
+            # "temperature": 0.7
+        },
+    )
+    response_json = response.json()
+    # print(response_json)
+    print(response_json["choices"][0]["message"]["content"], response_json["usage"])
+    return response_json["choices"][0]["message"]["content"]
+
+# 对比效果
+if __name__ == "__main__":
+    GPT_MODEL_LIST = [
+        "gpt-3.5-turbo",
+        # "gpt-4-turbo",
+        # "gpt-4",
+        # "gpt-3.5",
+    ]
+    for _ in range(3):
+        for each in range(len(GPT_MODEL_LIST)):
+            print(
+                GPT_MODEL_LIST[each],
+                no_matter_bot(message=PROMPT, prompt="", model=GPT_MODEL_LIST[each]),
+            )
+        print("*" * 10)
--- tik.py
+++ tik.py
@@ -1,5 +1,4 @@
 import os
-import random
 from WeiboBot import Bot
 from WeiboBot.message import Chat
 from WeiboBot.weibo import Weibo
@@ -10,43 +9,11 @@ import os
 import time
 from datetime import datetime
 
-from gpt.bot import no_matter_bot
+from GPT.api import *
 
 
 load_dotenv()
 myBot = Bot(cookies=os.getenv("BOT_COOKIES"))
-
-message_list = [
-    "没关系，能写几行代码已经很厉害了",
-    "没关系，今天也在学习英文，你是最棒的",
-    "没关系，能保持微笑已经很厉害了",
-    "没关系，能留些时间给自己独处，好厉害呀",
-    "没关系，一个人也能观赏电影，很厉害了",
-    "没关系，跑了几步已经很厉害了",
-    "没关系，能吃到自己喜欢的食物，真的很厉害了",
-    "没关系，认识到自己需要休息，这很厉害",
-    "没关系，在办公室又坚持了一天，很厉害了",
-    "没关系，能打开PowerPoint已经很厉害了",
-    "没关系，起身出门走了一小段，已经很厉害了",
-    "没关系，整理出一份待办清单，很厉害了",
-    "没关系，敢于面对自己的情绪，真的很厉害",
-    "没关系，给朋友打了个电话，这太厉害了",
-    "没关系，学习接受自己的不完美，很厉害了",
-    "没关系，能读一篇科技文章，很厉害了",
-    "没关系，对着镜子给自己微笑，你很棒",
-    "没关系，能舒舒服服的睡一觉，很厉害了",
-    "没关系，看了一本书，很好的",
-    "没关系，给自己下了个单，这很奇妙，很厉害",
-    "没关系，尝试学习画画已经很厉害了",
-    "没关系，能照顾自己，这真的很厉害",
-    "没关系，尽管沮丧，你还在持续，这真的很厉害",
-]
-
-
-@myBot.onNewMsg  # 被私信的时候触发
-async def on_msg(chat: Chat):
-    for msg in chat.msg_list:  # 消息列表
-        print(f"{msg.sender_screen_name}:{msg.text}")
 
 
 @myBot.onNewWeibo  # 首页刷到时触发
@@ -55,16 +22,27 @@ async def on_weibo(weibo: Weibo):
         print(f"{weibo.text}")
 
 
+@myBot.onNewMsg  # 被私信的时候触发
+async def on_msg(chat: Chat):
+    for msg in chat.msg_list:  # 消息列表
+        print(f"{msg.sender_screen_name}:{msg.text}")
+        reply = no_matter_bot(msg.text, prompt=REPLY_PROMPT)
+        await myBot.send_message(msg.sender_id, reply)
+        print(msg.text, reply)
+
+
 @myBot.onMentionCmt  # 提及我的评论时触发
 async def on_mention_cmt(cmt: Comment):
-    print(cmt)
+    reply = no_matter_bot(cmt.text)
+    await myBot.comment_weibo(cmt.mid, reply)
+    print(cmt.text, reply)
 
 
 @myBot.onTick  # 每次循环触发
 async def on_tick():
-    if datetime.now().minute == 30:
+    if datetime.now().minute == 18:
         print("hit tik, post now")
-        await myBot.post_weibo(random.choice(message_list))
+        await myBot.post_weibo(no_matter_bot(message=AUTO_PROMPT, prompt=''))
         time.sleep(60)
 
 
